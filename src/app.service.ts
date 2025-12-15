1946f95fb1f05b405cc8c31e76fc6e95cb5019e3
Yougal kumar <kyougal70@gmail.com>
2025-12-15 21:09:02 +0900

    add cron

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  @Cron(CronExpression.EVERY_30_SECONDS)
+  checkEvery30sec() {
+    // console.log('cron');
+    fetch('https://fb-news-b.onrender.com/hello').then((response) => {
+      console.log('s');
+      return true;
+    });
   }
 }
